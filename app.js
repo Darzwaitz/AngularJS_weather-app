3108462871aef0bbda6280997e9600fb6b5d0ae9
Darz_K <darrenkellydk@rogers.com>
2019-09-03 23:10:21 +0100

    experimenting with outputting in view, returned JSON values

--- app.js
+++ app.js
@@ -16,11 +16,37 @@ weatherApp.config(function ($routeProvider){
     })
 });
 
+// servicez
+weatherApp.service('cityService', function(){
+    this.city = 'Dublin';
+});
+
 //controllerz
-weatherApp.controller('homeController',['$Scope', function($Scope) {
+weatherApp.controller('homeController', ['$scope', 'cityService', 
+    function ($scope, cityService) {
+
+        $scope.city = cityService.city;
+
+        // set initial value in text box and watch for change
+        $scope.$watch('city', function(){
+            cityService.city = $scope.city;
+        });
 
 }]);
 
-weatherApp.controller('forecastController', ['$Scope', function ($Scope) {
+weatherApp.controller('forecastController', ['$scope', '$resource','cityService', 
+    function ($scope, $resource, cityService) {
+
+        $scope.city = cityService.city;
+
+        $scope.weatherAPI =
+            $resource('http://api.openweathermap.org/data/2.5/weather?q=dublin&APPID=3074798bebbc48aa3f502c48f6b00ad4' , {
+                callback: 'JSON_CALLBACK' }, { get: { method: 'JSONP'}});
+        $scope.weatherResult = $scope.weatherAPI.get({ 
+            q: $scope.city
+        });
+        
+        console.log($scope.weatherResult);
+        
 
 }]);
